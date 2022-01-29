3af24669ad32c461095ddd62fadef41dce450d29
ManshengChen <48541368+ManshengChen@users.noreply.github.com>
2022-01-29 11:29:34 +0800

    Add files via upload

---
+++ algo_OMSC.m
@@ -0,0 +1,116 @@
+function [A,W,Z,G,F,iter,obj,alpha] = algo_OMSC(X,Y,lambda,d,numanchor)
+% m      : the number of anchor. the size of Z is m*n.
+% lambda : the hyper-parameter of regularization term.
+% X      : n*di
+
+%% initialize
+maxIter = 50 ; % the number of iterations
+
+m = numanchor;
+numclass = length(unique(Y));
+numview = length(X);
+numsample = size(Y,1);
+
+W = cell(numview,1);    % di * d
+A = zeros(d,m);         % d  * m
+Z = zeros(m,numsample); % m  * n
+
+for i = 1:numview
+   di = size(X{i},1); 
+   W{i} = zeros(di,d);
+   X{i} = mapstd(X{i}',0,1); % turn into d*n
+end
+Z(:,1:m) = eye(m);
+%Initilize G,F
+G = eye(m,numclass);
+F = eye(numclass,numsample); 
+
+
+alpha = ones(1,numview)/numview;
+opt.disp = 0;
+
+flag = 1;
+iter = 0;
+%%
+while flag
+    iter = iter + 1;
+    
+    %% optimize W_i
+    AZ = A*Z; % since each view share the same A and Z.
+    parfor iv=1:numview
+        C = X{iv}*AZ';      
+        [U,~,V] = svd(C,'econ');
+        W{iv} = U*V';
+    end
+
+    %% optimize A
+    sumAlpha = 0;
+    part1 = 0;
+    for ia = 1:numview
+        al2 = alpha(ia)^2;
+        sumAlpha = sumAlpha + al2;
+        part1 = part1 + al2 * W{ia}' * X{ia} * Z';
+    end
+    [Unew,~,Vnew] = svd(part1,'econ');
+%     A = (part1/sumAlpha) * inv(Z*Z');
+    A = Unew*Vnew';
+    % Replace inv(A)*b with A\b
+    % Replace b*inv(A) with b/A
+    
+    %% optimize Z
+    % % QP
+    H = 2*sumAlpha*eye(m)+2*lambda*eye(m);
+    H = (H+H')/2;
+
+    options = optimset( 'Algorithm','interior-point-convex','Display','off'); % interior-point-convex
+    parfor ji=1:numsample
+        ff=0;
+        e = F(:,ji)'*G';
+        for j=1:numview
+            C = W{j} * A;
+            ff = ff - 2*X{j}(:,ji)'*C - 2*lambda*e;
+        end
+        Z(:,ji) = quadprog(H,ff',[],[],ones(1,m),1,zeros(m,1),ones(m,1),[],options);
+    end
+
+    %% optimize G
+    J = Z*F';      
+    [Ug,~,Vg] = svd(J,'econ');
+    G = Ug*Vg';
+    
+    %% optimize F
+    F=zeros(numclass,numsample);
+    for i=1:numsample
+        Dis=zeros(numclass,1);
+        for j=1:numclass
+            Dis(j)=(norm(Z(:,i)-G(:,j)))^2;
+        end
+        [~,r]=min(Dis);
+        F(r(1),i)=1;
+    end
+
+    %% optimize alpha
+    M = zeros(numview,1);
+    for iv = 1:numview
+        M(iv) = norm( X{iv} - W{iv} * A * Z,'fro')^2;
+    end
+    Mfra = M.^-1;
+    Q = 1/sum(Mfra);
+    alpha = Q*Mfra;
+
+    %%
+    term1 = 0;
+    for iv = 1:numview
+        term1 = term1 + alpha(iv)^2 * norm(X{iv} - W{iv} * A * Z,'fro')^2;
+    end
+    term2 = lambda * norm(Z - G * F,'fro')^2;
+    obj(iter) = term1+ term2;
+    
+    
+    if (iter==15) 
+        flag = 0;
+    end
+end
+         
+         
+    
---
+++ demo.m
@@ -0,0 +1,56 @@
+clear;
+clc;
+warning off;
+addpath(genpath('./'));
+
+%% dataset
+ds = {'Caltech101-20'};
+
+dsPath = './Dataset/';
+resPath = './res-lmd0/';
+metric = {'ACC','nmi','Purity','Fscore','Precision','Recall','AR','Entropy'};
+
+for dsi = 1
+    % load data & make folder
+    dataName = ds{dsi}; disp(dataName);
+    load(strcat(dsPath,dataName));
+    k = length(unique(Y));
+    
+    matpath = strcat(resPath,dataName);
+    txtpath = strcat(resPath,strcat(dataName,'.txt'));
+    if (~exist(matpath,'file'))
+        mkdir(matpath);
+        addpath(genpath(matpath));
+    end
+    dlmwrite(txtpath, strcat('Dataset:',cellstr(dataName), '  Date:',datestr(now)),'-append','delimiter','','newline','pc');
+    
+    %% para setting
+    anchor = [k 2*k 3*k];
+    d = k;
+    lambda = [1 0.1 0.01 0.001];
+    
+    %%
+    for ichor = 1:length(anchor)
+        for id = 1:length(d)
+            for j = 1:length(lambda)
+            tic;
+            [A,W,Z,G,F,iter,obj,alpha] = algo_OMSC(X,Y,lambda(j),d(id),anchor(ichor)); % X,Y,lambda,d,numanchor
+            [~,idx]=max(F);
+            res = Clustering8Measure(Y,idx); % [ACC nmi Purity Fscore Precision Recall AR Entropy]
+            timer(ichor,id)  = toc;
+            fprintf('Lambda:%f \t Anchor:%d \t Dimension:%d\t Res:%12.6f %12.6f %12.6f %12.6f \tTime:%12.6f \n',[lambda anchor(ichor) d(id) res(1) res(2) res(3) res(4) timer(ichor,id)]);
+            
+            resall{ichor,id} = res;
+            objall{ichor,id} = obj;
+            
+            dlmwrite(txtpath, [lambda(j) anchor(ichor) d(id) res timer(ichor,id)],'-append','delimiter','\t','newline','pc');
+            matname = ['_Anch_',num2str(anchor(ichor)),'_Dim_',num2str(d(id)),'.mat'];
+
+            save([matpath,'/',matname],'Z','A','W','alpha');
+            end
+        end
+    end
+    clear resall objall X Y k
+end
+
+
